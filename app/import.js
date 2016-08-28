67322f39515125cab685a027bdea2587469b7620
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2016-08-28 22:02:37 +0530

    introduce async in import

--- app/import.js
+++ app/import.js
@@ -8,21 +8,39 @@ const dburl = config.db;
 const db = mongojs(dburl);
 const movies = db.collection('movies');
 
-movies.drop()
 
-fs.readFile('config/seed.json', 'utf8', function(err, contents) {
-	if(err) console.log(err);
-	movies.insert(JSON.parse(contents).data, function(err, docs) {
-		if(err)
-			console.log(err);
-		else {
-			console.log('insertion of seed data success.');
-			movies.find(function(err, docs) {
-				console.log(docs);
-			});
-		}
-	})
-});
+async.series([
+	function(callback) {
+		movies.drop(function(err, result) {
+			if(err)
+				console.log(err);
+			else {
+				console.log(result);
+				console.log('movies collection dropped');
+			}
+		});
+		callback();
+	},
+	function(callback) {
+		fs.readFile('config/seed.json', 'utf8', function(err, contents) {
+			if(err) console.log(err);
+			movies.insert(JSON.parse(contents).data, function(err, docs) {
+				if(err)
+					console.log(err);
+				else {
+					console.log('insertion of seed data success');
+				}
+				callback();
+			})
+		});		
+	},
+	function(callback) {
+		movies.find(function(err, docs) {
+			console.log('movies collection contains the following documents now:');
+			console.log(docs);
+		});
+	}
+]);
 
 console.log('running import file');
 console.log('db url: ' + dburl);
